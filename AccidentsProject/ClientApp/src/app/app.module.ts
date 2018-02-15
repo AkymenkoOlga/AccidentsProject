cce6d58182281d0539fd9c890e03fbc4e277766d
Olga Akymenko <o.akymenko@gmail.com>
2018-02-15 14:39:22 +0100

    Removed default pages

--- AccidentsProject/ClientApp/src/app/app.module.ts
+++ AccidentsProject/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AccidentsComponent } from './accidents/accidents.component';
 import { MapComponent } from './map/map.component';
 
@@ -17,8 +15,6 @@ import { MapComponent } from './map/map.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     AccidentsComponent,
     MapComponent
   ],
@@ -28,8 +24,6 @@ import { MapComponent } from './map/map.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'accidents', component: AccidentsComponent },
       { path: 'map', component: MapComponent }
     ])
