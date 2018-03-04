f3c05735dee82ee2525df28ddca2be85d872554d
Olga Akymenko <o.akymenko@gmail.com>
2018-03-04 13:49:49 +0100

    Added pagination to the accidents table

--- AccidentsProject/ClientApp/src/app/accidents/accidents.component.ts
+++ AccidentsProject/ClientApp/src/app/accidents/accidents.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 import Appcomponent = require("../app.component");
 import Accident = Appcomponent.Accident;
@@ -13,7 +14,9 @@ export class AccidentsComponent implements OnInit {
   public accidents: Accident[];
 
   displayedColumns = ['id', 'date', "lat", "lon", 'tags'];
-  dataSource = this.accidents;
+  dataSource = new MatTableDataSource<Accident>(this.accidents);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Accident[]>(baseUrl + 'api/accidents').subscribe(result => {
@@ -26,6 +29,7 @@ export class AccidentsComponent implements OnInit {
   }
 
   refresh() {
-    this.dataSource = this.accidents;
+    this.dataSource = new MatTableDataSource<Accident>(this.accidents);
+    this.dataSource.paginator = this.paginator;
   }
 }
