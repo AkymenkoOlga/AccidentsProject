6e18156096f4560dff0c92028ee8df249e289923
Olga Akymenko <o.akymenko@gmail.com>
2018-03-04 11:32:13 +0100

    Added default entries to MongoDb (reset and push N accidents) [temp]

--- AccidentsProject/Repositories/MongoDb/AccidentRepository.cs
+++ AccidentsProject/Repositories/MongoDb/AccidentRepository.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AccidentsProject.Repositories.Entities;
 using AccidentsProject.Repositories.MongoDb;
@@ -16,6 +16,33 @@ namespace AccidentsProject.Repositories
         public AccidentRepository(IMongoClient client)
             : base(client, DatabaseName, CollectionName)
         {
+            // TODO: remove once there is a real data in MongoDB
+            client.DropDatabase(DatabaseName);
+            this.Reset().Wait();
+        }
+
+        private async Task Reset()
+        {
+            var tags = new[] { "day", "night", "fatal", "bicycle", "motorcycle", "automobile" };
+
+            var x = 0;
+            do
+            {
+                await this.InsertOneAsync(new AccidentEntity()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Date = DateTime.Now.ToUniversalTime(),
+                    Location = new LocationEntity() { Latitude = GetRandomNumber(49.0, 49.5), Longitude = GetRandomNumber(9.0, 9.5) },
+                    Tags = tags.OrderBy(arg => Guid.NewGuid()).Take(2).ToList()
+                });
+                x++;
+            } while (x < 10);
+        }
+
+        private static double GetRandomNumber(double minimum, double maximum)
+        {
+            var random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
         }
     }
 }
