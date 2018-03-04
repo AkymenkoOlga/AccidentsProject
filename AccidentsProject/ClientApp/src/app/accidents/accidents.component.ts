cf30c94437cb43cbdebdac752b948db34ef20f9b
Olga Akymenko <o.akymenko@gmail.com>
2018-03-04 13:05:28 +0100

    Intermediate table changes

--- AccidentsProject/ClientApp/src/app/accidents/accidents.component.ts
+++ AccidentsProject/ClientApp/src/app/accidents/accidents.component.ts
@@ -1,11 +1,13 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-accidents',
   templateUrl: './accidents.component.html'
 })
-export class AccidentsComponent {
+
+export class TablePaginationExample {
   public accidents: Accident[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
@@ -13,10 +15,23 @@ export class AccidentsComponent {
       this.accidents = result;
     }, error => console.error(error));
   }
+  displayedColumns = ['id', 'date', 'severity'];
+  dataSource = new MatTableDataSource<Accident>(this.accidents);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
 
-interface Accident {
+
+export interface Accident {
   id: string;
   date: Date;
   severity: string;
 }
+
+
+
+
+
